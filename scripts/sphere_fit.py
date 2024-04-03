40ea7e42a3272226f84c092768badbd32b21876f
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-04-03 18:14:42 -0400

    Update sphere_fit.py

--- scripts/sphere_fit.py
+++ scripts/sphere_fit.py
@@ -15,14 +15,24 @@ import numpy as np
 import math 
 from robot_vision_lectures.msg import XYZarray, SphereParams
 
+
+filter_gains = {'xc': 0.1, 'yc': 0.1, 'zc': 0.1, 'radius': 0.1}
+matrix_a = []
+matrix_b = []
+output = True
+xOutput, yOutput, zOutput, rOutput = 0.0, 0.0, 0.0, 0.0
+valParams = False
+
 ###builds matrices from receiving the data points
-def build_matrices(data_points,matrix_a, matrix_b):
+def build_matrices(data_points):
+	global matrix_a
+	global matrix_b
 	###create matrices from Points array
 	for i in data_points.points:
 		###[2*x, 2*y, 2*z, 1]
-		matrix_a.append([2 * point.x, 2 * point.y, 2 * point.z, 1])
+		matrix_a.append([2 * i.x, 2 * i.y, 2 * i.z, 1])
 		###[x^2 + y^2 + z^2]
-		matrix_b.append([point.x ** 2 + point.y ** 2 + point.z ** 2])
+		matrix_b.append([i.x ** 2 + i.y ** 2 + i.z ** 2])
 ###fits the sphere model to data points and finds P
 def fit(matrix_a, matrix_b):
 	unfiltered = SphereParams()
@@ -33,14 +43,14 @@ def fit(matrix_a, matrix_b):
 	
 	try: 
 		##used to find P
-		P = np.linalg.lstsq(A, B, condition = None)[0]
+		P = np.linalg.lstsq(A, B, rcond = None)[0]
 		###set unfiltered center and radius parameters
 		unfiltered.xc = P[0]
 		unfiltered.yc = P[1]
 		unfiltered.zc = P[2]
 		### radius calculation: SQRT(P[3] + Xc^2 + Yc^2 + Zc^2)
 		unfiltered.radius = math.sqrt(P[3] + P[0] ** 2 + P[1] ** 2 + P[2] ** 2)
-		valParams = True
+	
 		### return the parameter message
 		return unfiltered
 		
@@ -80,14 +90,12 @@ if __name__ == '__main__':
 	#init ros node
 	rospy.init_node('sphere_fit', anonymous = True)
 	#init all variables
-	matrix_a = [] 
-	matrix_b=[]
 	output = True 
 	xOutput, yOutput, zOutput, rOutput = 0.0, 0.0, 0.0, 0.0 
 	valParams = False
 	# define subscriber and publisher
 	# subscribe to /xyz_croppedtopic
-	img_sub = rospy.Subscriber("/xyz_cropped", XYZarray, build_matrices, (matrix_a, matrix_b)) 
+	img_sub = rospy.Subscriber("/xyz_cropped_ball", XYZarray, build_matrices) 
 	#create publisher sphereParameters on the /sphere_params topic
 	# define a publisher to publish images
 	sp_pub = rospy.Publisher('/sphere_params', SphereParams, queue_size = 10)
