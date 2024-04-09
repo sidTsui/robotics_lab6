dba39be7cebe6842334a0fdd33eb34e965c7e8c8
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-04-09 11:50:15 -0400

    Update sphere_fit.py

--- scripts/sphere_fit.py
+++ scripts/sphere_fit.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ###Sidney Tsui
 ###lab 6
 ### 4/2/2024
@@ -9,7 +11,6 @@
 #use method explained in class to implement low-pass filter to each parameter.
 #find proper filter gains and fine tune them
 
-#!/usr/bin/env python3
 import rospy
 import numpy as np
 import math 
@@ -17,6 +18,8 @@ from robot_vision_lectures.msg import XYZarray, SphereParams
 
 
 filter_gain = {'xc': 0.1, 'yc': 0.1, 'zc': 0.1, 'radius': 0.1}
+
+
 matrix_a = []
 matrix_b = []
 output = True
@@ -35,6 +38,7 @@ def build_matrices(data_points):
 		matrix_b.append([i.x ** 2 + i.y ** 2 + i.z ** 2])
 ###fits the sphere model to data points and finds P
 def fit(matrix_a, matrix_b):
+	global valParams
 	unfiltered = SphereParams()
 
 	A = np.array(matrix_a)
@@ -50,7 +54,7 @@ def fit(matrix_a, matrix_b):
 		unfiltered.zc = P[2]
 		### radius calculation: SQRT(P[3] + Xc^2 + Yc^2 + Zc^2)
 		unfiltered.radius = math.sqrt(P[3] + P[0] ** 2 + P[1] ** 2 + P[2] ** 2)
-	
+		valParams = True
 		### return the parameter message
 		return unfiltered
 		
@@ -76,23 +80,24 @@ def filtered(unfiltered, filter_gain, output, xOutput, yOutput, zOutput, rOutput
 	rInput = unfiltered.radius
 	###from Input
 
-	return filParams
+	xOutput = filter_gain.get('xc') * xInput + (1 - filter_gain.get('xc')) * xOutput
+	yOutput = filter_gain.get('yc') * yInput + (1 - filter_gain.get('yc')) * yOutput
+	zOutput = filter_gain.get('zc') * zInput + (1 - filter_gain.get('zc')) * zOutput
+	rOutput = filter_gain.get('radius') * rInput + (1 - filter_gain.get('radius')) * rOutput
+	
+	filParams.xc = xOutput
+	filParams.yc = yOutput
+	filParams.zc = zOutput
+	filParams.radius = rOutput
+
+	return filParams	
 	
 if __name__ == '__main__':
 	#init ros node
 	rospy.init_node('sphere_fit', anonymous = True)
 	#init all variables
 	output = True 
-	xOutput, yOutput, zOutput, rOutput = -0.14, -0.2, 0.5, 0.05	
-	filParams.xc = xOutput
-	filParams.yc = yOutput
-	filParams.zc = zOutput
-	filParams.radius = rOutput
-	xOutput = filter_gain * xInput + (1 - filter_gain) * xOutput
-	yOutput = filter_gain * yInput + (1 - filter_gain) * yOutput
-	zOutput = filter_gain * zInput + (1 - filter_gain) * zOutput
-	rOutput = filter_gain * rInput + (1 - filter_gain) * rOutput
-	
+	xOutput, yOutput, zOutput, rOutput = 0.14, -0.2, 0.5, 0.05	
 	valParams = False
 	# define subscriber and publisher
 	# subscribe to /xyz_croppedtopic
@@ -112,4 +117,5 @@ if __name__ == '__main__':
 			    #check P empty
 				# publish sphere params
 				sp_pub.publish(filParams)
+				
 		rate.sleep()
